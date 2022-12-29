db96d2296bfee9ae5003107d5d2ceec439e48d9a
Daniel Silva <daniel.silva.prg@gmail.com>
2022-12-29 20:04:15 +0000

    added search box to look for city

--- ReviewsService/convertLocation.js
+++ ReviewsService/convertLocation.js
@@ -1,3 +1,17 @@
-exports.cityToLatlng = function(city){
+const geo = require("node-geocoder")
 
+var options = {
+  provider: 'google',
+  httpAdapter: 'https', // Default
+  apiKey: 'AIzaSyBq2YyQh70n_M6glKgr3U4a9vCmY5LU0xQ', // for Mapquest, OpenCage, Google Premier
+  formatter: 'json' // 'gpx', 'string', ...
+};
+
+var geocoder = geo(options);
+
+exports.cityToLatLng = async function(city){
+	var res = await geocoder.geocode(city)
+	//console.log(city, res)
+	
+	return res 
 }
