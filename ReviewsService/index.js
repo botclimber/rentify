e5dcafa1d08a6e14ddb9d30f9a7bf6aca41a06a4
Daniel Silva <daniel.silva.prg@gmail.com>
2022-12-29 18:31:38 +0000

    update of readme files

--- ReviewsService/index.js
+++ ReviewsService/index.js
@@ -3,6 +3,22 @@ const { Server } = require('ws');
 
 const sockserver = new Server({ port: 8000});
 const connections = new Set();
+//const conv = require("convertLocation.js")
+
+var revMsg = '<div id="content">' +
+'<div id="siteNotice">' +
+"</div>" +
+'<h1 id="firstHeading" class="firstHeading">Reviews</h1>' +
+'<div id="bodyContent">'+
+"<ul>" +
+"<li>review 0</li>" +
+"<li>review 1</li>" +
+"<li>review 2</li>" +
+"<li>review 3</li>" +
+"<li>review 4</li>" +
+"</ul>" +
+"</div>" +
+"</div>";
 
 sockserver.on('connection', (ws) => {
    console.log('New client connected!');
@@ -11,118 +27,14 @@ sockserver.on('connection', (ws) => {
         var locations = {
           type: "locations",
           locations: {
-          test0: { lat: -33.8567844, lng: 151.213108, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 0</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test1: { lat: -33.8472767, lng: 151.2188164, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 1</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test2: { lat: -33.8209738, lng: 151.2563253, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 2</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test3: { lat: -33.8690081, lng: 151.2052393, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 3</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test4: { lat: -33.8587568, lng: 151.2058246, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 4</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test5: { lat: -33.858761, lng: 151.2055688, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 5</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test6: { lat: -33.852228, lng: 151.2038374, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 6</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" },
-          test7: { lat: -33.8737375, lng: 151.222569, reviews: '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">test 7</h1>' +
-    '<div id="bodyContent">'+
-    "<ul>" +
-    "<li>review 0</li>" +
-    "<li>review 1</li>" +
-    "<li>review 2</li>" +
-    "<li>review 3</li>" +
-    "<li>review 4</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" }
+          test0: { lat: -33.8567844, lng: 151.213108, reviews:revMsg  },
+          test1: { lat: -33.8472767, lng: 151.2188164, reviews: revMsg },
+          test2: { lat: -33.8209738, lng: 151.2563253, reviews: revMsg },
+          test3: { lat: -33.8690081, lng: 151.2052393, reviews: revMsg },
+          test4: { lat: -33.8587568, lng: 151.2058246, reviews: revMsg },
+          test5: { lat: -33.858761, lng: 151.2055688, reviews: revMsg },
+          test6: { lat: -33.852228, lng: 151.2038374, reviews: revMsg },
+          test7: { lat: -33.8737375, lng: 151.222569, reviews: revMsg }
         }
       };
 
