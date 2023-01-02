a09c1537b6be22e282ab9d77bad1af714e011c33
Daniel Silva <daniel.silva.prg@gmail.com>
2023-01-02 22:42:22 +0000

    some thought about code enchantment

---
+++ ReviewsService/src/Review.js
@@ -0,0 +1,14 @@
+// thinking about best approach
+
+module.exports = class Review{
+	//Todo
+}
+
+exports.review =function(city, ) {
+	city: city,
+	street: street,
+	bNr: bNr,
+	create: function(){},
+	add: function(){},
+	get: function(){}
+}
