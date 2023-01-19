a9c622e32de8f916503c91b735f166c48cdccdb7
Daniel Silva <daniel.silva.prg@gmail.com>
2023-01-19 00:38:36 +0000

    wip: log messages

--- ReviewsService/src/actions.js
+++ ReviewsService/src/actions.js
@@ -8,26 +8,30 @@ exports.actions = (function(ws){
 
 	// ACTION TO SEARCH FOR ADDRESS
 	function address(data){
-		console.log(ws, data)
+		console.info("Starting address action for "+ws+" client containing following data: "+data)
 		conv.getLatLng(data)
         .then(res => {
 
+					console.info("Getting all reviews ...")
 					var allReviews = Reviews.getAll()
 					console.log(allReviews)
 
+					console.info("assembling response ...")
 					var response = {
 						type: data.type,
 						address: {lat: res[0].latitude, lng: res[0].longitude},
 						locations: JSON.parse(allReviews.locations())
 					}
+					console.log(response)
 
+					console.log("sending response to client")
 					ws.forEach((client) => {
 						 client.send(JSON.stringify(response));
 					});
 
         }).catch( reason => {
 
-					console.log(reason)
+					console.log("Couldnt handle request "+reason)
 					ws.forEach((client) => {
 						 client.send(JSON.stringify({status: "rejected",msg: reason}));
 					});
@@ -36,6 +40,13 @@ exports.actions = (function(ws){
 
 	// ACTION TO INSERT/CREATE NEW REVIEW
 	function createReview(data){
+
+		/**
+		assembles response and send it to client
+
+		@lat
+		@lng
+		*/
 		function run(lat,lng){
 
 			// creating new review record
@@ -58,12 +69,15 @@ exports.actions = (function(ws){
 			});
 		}
 
-		if(data.lat !== "" && data.lng !== "") run(data.lat, data.lng)
-		else{
+		if(data.lat !== "" && data.lng !== ""){
+			console.info("Use of defined lat, lng ("+data.lat+", "+data.lng+")")
+			run(data.lat, data.lng)
+
+		}else{
+			console.info("Lat and Lng not defined ... requesting it by using address ("+data.city+", "+data.street+", "+data.buildingNumber+")")
 			conv.getLatLng(data)
 			.then(res => {
 
-				console.log(res)
 				run(res[0].latitude, res[0].longitude)
 
 			}).catch( reason => {
