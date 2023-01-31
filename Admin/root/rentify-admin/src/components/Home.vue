82d4c2846ee23ffcf7f61739b4bc2fca38e8b205
Daniel Silva <daniel.silva.prg@gmail.com>
2023-01-31 17:17:48 +0000

    wip: file clean up | starting to build home | for now aim is to setup reviews validator

--- Admin/root/rentify-admin/src/components/Home.vue
+++ Admin/root/rentify-admin/src/components/Home.vue
@@ -1,28 +1,6 @@
 <template>
       <div class="content-wrapper">
         <div class="row">
-          <div class="col-12 grid-margin stretch-card">
-            <div class="card corona-gradient-card">
-              <div class="card-body py-0 px-0 px-sm-3">
-                <div class="row align-items-center">
-                  <div class="col-4 col-sm-3 col-xl-2">
-                    <img src="../assets/images/dashboard/Group126@2x.png" class="gradient-corona-img img-fluid" alt="">
-                  </div>
-                  <div class="col-5 col-sm-7 col-xl-8 p-0">
-                    <h4 class="mb-1 mb-sm-0">Want even more features?</h4>
-                    <p class="mb-0 font-weight-normal d-none d-sm-block">Check out our Pro version with 5 unique layouts!</p>
-                  </div>
-                  <div class="col-3 col-sm-2 col-xl-2 pl-0 text-center">
-                    <span>
-                      <a href="https://www.bootstrapdash.com/product/corona-admin-template/" target="_blank" class="btn btn-outline-light btn-rounded get-started-btn">Upgrade to PRO</a>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
           <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
@@ -105,135 +83,6 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-4 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <h4 class="card-title">Transaction History</h4>
-                <canvas id="transaction-history" class="transaction-chart"></canvas>
-                <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
-                  <div class="text-md-center text-xl-left">
-                    <h6 class="mb-1">Transfer to Paypal</h6>
-                    <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p>
-                  </div>
-                  <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-                    <h6 class="font-weight-bold mb-0">$236</h6>
-                  </div>
-                </div>
-                <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
-                  <div class="text-md-center text-xl-left">
-                    <h6 class="mb-1">Tranfer to Stripe</h6>
-                    <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p>
-                  </div>
-                  <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-                    <h6 class="font-weight-bold mb-0">$593</h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-8 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <div class="d-flex flex-row justify-content-between">
-                  <h4 class="card-title mb-1">Open Projects</h4>
-                  <p class="text-muted mb-1">Your data status</p>
-                </div>
-                <div class="row">
-                  <div class="col-12">
-                    <div class="preview-list">
-                      <div class="preview-item border-bottom">
-                        <div class="preview-thumbnail">
-                          <div class="preview-icon bg-primary">
-                            <i class="mdi mdi-file-document"></i>
-                          </div>
-                        </div>
-                        <div class="preview-item-content d-sm-flex flex-grow">
-                          <div class="flex-grow">
-                            <h6 class="preview-subject">Admin dashboard design</h6>
-                            <p class="text-muted mb-0">Broadcast web app mockup</p>
-                          </div>
-                          <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p class="text-muted">15 minutes ago</p>
-                            <p class="text-muted mb-0">30 tasks, 5 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="preview-item border-bottom">
-                        <div class="preview-thumbnail">
-                          <div class="preview-icon bg-success">
-                            <i class="mdi mdi-cloud-download"></i>
-                          </div>
-                        </div>
-                        <div class="preview-item-content d-sm-flex flex-grow">
-                          <div class="flex-grow">
-                            <h6 class="preview-subject">Wordpress Development</h6>
-                            <p class="text-muted mb-0">Upload new design</p>
-                          </div>
-                          <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p class="text-muted">1 hour ago</p>
-                            <p class="text-muted mb-0">23 tasks, 5 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="preview-item border-bottom">
-                        <div class="preview-thumbnail">
-                          <div class="preview-icon bg-info">
-                            <i class="mdi mdi-clock"></i>
-                          </div>
-                        </div>
-                        <div class="preview-item-content d-sm-flex flex-grow">
-                          <div class="flex-grow">
-                            <h6 class="preview-subject">Project meeting</h6>
-                            <p class="text-muted mb-0">New project discussion</p>
-                          </div>
-                          <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p class="text-muted">35 minutes ago</p>
-                            <p class="text-muted mb-0">15 tasks, 2 issues</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="preview-item border-bottom">
-                        <div class="preview-thumbnail">
-                          <div class="preview-icon bg-danger">
-                            <i class="mdi mdi-email-open"></i>
-                          </div>
-                        </div>
-                        <div class="preview-item-content d-sm-flex flex-grow">
-                          <div class="flex-grow">
-                            <h6 class="preview-subject">Broadcast Mail</h6>
-                            <p class="text-muted mb-0">Sent release details to team</p>
-                          </div>
-                          <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p class="text-muted">55 minutes ago</p>
-                            <p class="text-muted mb-0">35 tasks, 7 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="preview-item">
-                        <div class="preview-thumbnail">
-                          <div class="preview-icon bg-warning">
-                            <i class="mdi mdi-chart-pie"></i>
-                          </div>
-                        </div>
-                        <div class="preview-item-content d-sm-flex flex-grow">
-                          <div class="flex-grow">
-                            <h6 class="preview-subject">UI Design</h6>
-                            <p class="text-muted mb-0">New application planning</p>
-                          </div>
-                          <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p class="text-muted">50 minutes ago</p>
-                            <p class="text-muted mb-0">27 tasks, 4 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
           <div class="col-sm-4 grid-margin">
             <div class="card">
               <div class="card-body">
@@ -293,7 +142,7 @@
           </div>
         </div>
         <div class="row ">
-          <div class="col-12 grid-margin">
+          <div class="col-8 grid-margin">
             <div class="card">
               <div class="card-body">
                 <h4 class="card-title">Order Status</h4>
@@ -429,117 +278,7 @@
               </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 col-xl-4 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <div class="d-flex flex-row justify-content-between">
-                  <h4 class="card-title">Messages</h4>
-                  <p class="text-muted mb-1 small">View all</p>
-                </div>
-                <div class="preview-list">
-                  <div class="preview-item border-bottom">
-                    <div class="preview-thumbnail">
-                      <img src="../assets/images/faces/face6.jpg" alt="image" class="rounded-circle" />
-                    </div>
-                    <div class="preview-item-content d-flex flex-grow">
-                      <div class="flex-grow">
-                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Leonard</h6>
-                          <p class="text-muted text-small">5 minutes ago</p>
-                        </div>
-                        <p class="text-muted">Well, it seems to be working now.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="preview-item border-bottom">
-                    <div class="preview-thumbnail">
-                      <img src="../assets/images/faces/face8.jpg" alt="image" class="rounded-circle" />
-                    </div>
-                    <div class="preview-item-content d-flex flex-grow">
-                      <div class="flex-grow">
-                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Luella Mills</h6>
-                          <p class="text-muted text-small">10 Minutes Ago</p>
-                        </div>
-                        <p class="text-muted">Well, it seems to be working now.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="preview-item border-bottom">
-                    <div class="preview-thumbnail">
-                      <img src="../assets/images/faces/face9.jpg" alt="image" class="rounded-circle" />
-                    </div>
-                    <div class="preview-item-content d-flex flex-grow">
-                      <div class="flex-grow">
-                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Ethel Kelly</h6>
-                          <p class="text-muted text-small">2 Hours Ago</p>
-                        </div>
-                        <p class="text-muted">Please review the tickets</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="preview-item border-bottom">
-                    <div class="preview-thumbnail">
-                      <img src="../assets/images/faces/face11.jpg" alt="image" class="rounded-circle" />
-                    </div>
-                    <div class="preview-item-content d-flex flex-grow">
-                      <div class="flex-grow">
-                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Herman May</h6>
-                          <p class="text-muted text-small">4 Hours Ago</p>
-                        </div>
-                        <p class="text-muted">Thanks a lot. It was easy to fix it .</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-xl-4 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <h4 class="card-title">Portfolio Slide</h4>
-                <div class="owl-carousel owl-theme full-width owl-carousel-dash portfolio-carousel" id="owl-carousel-basic">
-                  <div class="item">
-                    <img src="../assets/images/dashboard/Rectangle.jpg" alt="">
-                  </div>
-                  <div class="item">
-                    <img src="../assets/images/dashboard/Img_5.jpg" alt="">
-                  </div>
-                  <div class="item">
-                    <img src="../assets/images/dashboard/img_6.jpg" alt="">
-                  </div>
-                </div>
-                <div class="d-flex py-4">
-                  <div class="preview-list w-100">
-                    <div class="preview-item p-0">
-                      <div class="preview-thumbnail">
-                        <img src="../assets/images/faces/face12.jpg" class="rounded-circle" alt="">
-                      </div>
-                      <div class="preview-item-content d-flex flex-grow">
-                        <div class="flex-grow">
-                          <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                            <h6 class="preview-subject">CeeCee Bass</h6>
-                            <p class="text-muted text-small">4 Hours Ago</p>
-                          </div>
-                          <p class="text-muted">Well, it seems to be working now.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <p class="text-muted">Well, it seems to be working now. </p>
-                <div class="progress progress-md portfolio-progress">
-                  <div class="progress-bar bg-success" role="progressbar" style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-12 col-xl-4 grid-margin stretch-card">
+          <div class="col-md-4 col-xl-4 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
                 <h4 class="card-title">To do list</h4>
@@ -590,75 +329,6 @@
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-body">
-                <h4 class="card-title">Visitors by Countries</h4>
-                <div class="row">
-                  <div class="col-md-5">
-                    <div class="table-responsive">
-                      <table class="table">
-                        <tbody>
-                          <tr>
-                            <td>
-                              <i class="flag-icon flag-icon-us"></i>
-                            </td>
-                            <td>USA</td>
-                            <td class="text-right"> 1500 </td>
-                            <td class="text-right font-weight-medium"> 56.35% </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <i class="flag-icon flag-icon-de"></i>
-                            </td>
-                            <td>Germany</td>
-                            <td class="text-right"> 800 </td>
-                            <td class="text-right font-weight-medium"> 33.25% </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <i class="flag-icon flag-icon-au"></i>
-                            </td>
-                            <td>Australia</td>
-                            <td class="text-right"> 760 </td>
-                            <td class="text-right font-weight-medium"> 15.45% </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <i class="flag-icon flag-icon-gb"></i>
-                            </td>
-                            <td>United Kingdom</td>
-                            <td class="text-right"> 450 </td>
-                            <td class="text-right font-weight-medium"> 25.00% </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <i class="flag-icon flag-icon-ro"></i>
-                            </td>
-                            <td>Romania</td>
-                            <td class="text-right"> 620 </td>
-                            <td class="text-right font-weight-medium"> 10.25% </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <i class="flag-icon flag-icon-br"></i>
-                            </td>
-                            <td>Brasil</td>
-                            <td class="text-right"> 230 </td>
-                            <td class="text-right font-weight-medium"> 75.00% </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                  <div class="col-md-7">
-                    <div id="audience-map" class="vector-map"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
 </template>
