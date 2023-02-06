cfad0d0f6c243a3324d1084805d0d5c14e9f543a
walter palma <walterrdcpalma@gmail.com>
2023-02-06 01:32:01 +0000

    login simulator

--- Services/Users/routes/routing.js
+++ Services/Users/routes/routing.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../modules/User.js');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     res.json("Ola");
@@ -8,12 +9,37 @@ router.get('/', (req, res) => {
 
 router.use(express.json());
 
-router.post('/', (req, res) => {
-    //const user = new User(req.body.username, req.body.password);
-    const user = { username: req.body.username, password: req.body.password }
+const users = [];
+
+
+router.post('/', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        //const user = new User(req.body.username, req.body.password);
+        users.push({ username: req.body.username, password: hashedPassword });
+        console.log(users);
+        //user.insert(user);
+    } catch {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    console.log(users);
+    const user = users.find(user => user.username === req.body.username);
     console.log(user);
-    //user.insert(user);
-    res.status(201);
+    if (user == null) {
+        return res.status(404).json({ message: "User not found" })
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Failed')
+        }
+    } catch {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 })
 
 module.exports = router;
