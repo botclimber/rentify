8ca8e7ecd135768c80c05beaec2ab81d5d164e44
walter palma <walterrdcpalma@gmail.com>
2023-02-11 03:01:52 +0000

    draft login using vue.js

---
+++ clients/userclient/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <input type="text" v-model="email" placeholder="Enter Email" />
+      <br />
+      <input type="password" v-model="password" placeholder="Enter Password" />
+      <br />
+      <button v-on:click="login">Login</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.email, this.password);
+    },
+  },
+};
+</script>
+<style></style>
---
+++ clients/userclient/src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Login Page</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Login-Page",
+});
+</script>
+<style></style>
