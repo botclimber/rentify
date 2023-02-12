64be4057f0f2cf0d50d2c51fd913682ed4f7e66a
walter palma <walterrdcpalma@gmail.com>
2023-02-12 01:27:28 +0000

    redirect button to login page

--- clients/userclient/src/components/Register.vue
+++ clients/userclient/src/components/Register.vue
@@ -1,61 +1,77 @@
 <template>
   <div class="container" style="margin-top: 60px; width: 30%">
-    <form>
-      <!-- Name input -->
-      <div class="form-outline mb-4">
-        <input
-          type="text"
-          id="form2Example1"
-          class="form-control"
-          v-model="name"
-          placeholder="Name"
-        />
-      </div>
-
-      <!-- Username input -->
-      <div class="form-outline mb-4">
-        <input
-          type="text"
-          id="form2Example1"
-          class="form-control"
-          v-model="username"
-          placeholder="Username"
-        />
-      </div>
-
-      <!-- Email input -->
-      <div class="form-outline mb-4">
-        <input
-          type="email"
-          id="form2Example1"
-          class="form-control"
-          v-model="email"
-          placeholder="Email"
-        />
-      </div>
-
-      <!-- Password input -->
-      <div class="form-outline mb-4">
-        <input
-          type="password"
-          id="form2Example2"
-          class="form-control"
-          v-model="password"
-          placeholder="Password"
-        />
-      </div>
+    <div v-if="isRegistered">
+      User registered successfully. Check your email to verify your account
 
       <div class="row mb-4">
         <!-- Submit button -->
         <button
           type="button"
           class="btn btn-primary btn-md btn-block"
-          v-on:click="register"
+          v-on:click="goToLogin"
         >
-          Register
+          Redirect to login page
         </button>
       </div>
-    </form>
+    </div>
+    <div v-else>
+      <form>
+        <!-- Name input -->
+        <div class="form-outline mb-4">
+          <input
+            type="text"
+            id="form2Example1"
+            class="form-control"
+            v-model="name"
+            placeholder="Name"
+          />
+        </div>
+
+        <!-- Username input -->
+        <div class="form-outline mb-4">
+          <input
+            type="text"
+            id="form2Example1"
+            class="form-control"
+            v-model="username"
+            placeholder="Username"
+          />
+        </div>
+
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+          <input
+            type="email"
+            id="form2Example1"
+            class="form-control"
+            v-model="email"
+            placeholder="Email"
+          />
+        </div>
+
+        <!-- Password input -->
+        <div class="form-outline mb-4">
+          <input
+            type="password"
+            id="form2Example2"
+            class="form-control"
+            v-model="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <div class="row mb-4">
+          <!-- Submit button -->
+          <button
+            type="button"
+            class="btn btn-primary btn-md btn-block"
+            v-on:click="register"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -71,6 +87,7 @@ export default defineComponent({
       username: "",
       email: "",
       password: "",
+      isRegistered: false,
     };
   },
   methods: {
@@ -81,12 +98,16 @@ export default defineComponent({
         this.name,
         this.username
       )
-        .then(function (response) {
+        .then((response) => {
           console.log(response.data);
+          this.isRegistered = true;
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.error(error);
         });
+    },
+    goToLogin() {
+      this.$router.push({ name: "Login-Form" });
     },
   },
 });
